c799fec4d06f11ef8a5edf5b1de017778cdb495c
lsdjjong <lsdjjong@naver.com>
2019-06-18 13:30:31 +0900

    구구단 소스

--- index.php
+++ index.php
@@ -4,3 +4,25 @@
         <h1>hello world</h1>
     </body>    
 </html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<?php
+ 
+echo "<h1>Ex. 구구단 출력하기</h1>";
+ 
+for($i=2;$i<=9;$i++){
+    echo $i."단 입니다.<br/>";
+    for($j=1;$j<=9;$j++){
+        echo $i." X ".$j." = ".($i*$j)."<br/>";
+    }
+    echo "<br/>";
+}
+ 
+?>
+</body>
+</html>
